64c3d7773dc72939afd27286589945ee39a03019
Okusindja Rodrigues de Almeida <okusindja@gmail.com>
2026-05-22 09:43:08 +0100

    ✨ feat: add @vercel/analytics integration and update dependencies

--- src/components/client-providers.tsx
+++ src/components/client-providers.tsx
@@ -3,11 +3,13 @@
 import { ReactNode } from 'react';
 import { LocaleProvider } from '@/components/locale-provider';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from "@vercel/analytics/next"
 
 export function ClientProviders({ children }: { children: ReactNode }) {
   return (
     <LocaleProvider>
       <ThemeProvider>
+        <Analytics />
         {children}
       </ThemeProvider>
     </LocaleProvider>
